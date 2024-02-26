19732bb030bb32a2df120a2be4458ddfce7b8546
ItzIcyGaming <73133185+TrulyItzIcyGaming@users.noreply.github.com>
2024-02-26 16:38:13 -0500

    Add files via upload

---
+++ py.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+(function() {
+  var oScript = document.createElement('app.py');
+  oScript.type = 'text/javascript';
+  oScript.async = true;
+  oScript.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'example.com/yourscript.js';
+  var oParent = document.getElementsByTagName('script')[0];
+  oParent.parentNode.insertBefore(oScript, oParent);
+})();
+</script>
